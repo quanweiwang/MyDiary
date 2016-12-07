54ffa4c92ac23e987400b1a912153eb720e6f5ad
admin <wangquanwei@wangquanweidemacbook-pro-2.local>
2016-12-07 10:53:19 +0800

    Initial Commit

---
+++ MyDiary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyDiary
+//
+//  Created by 王权伟 on 2016/12/7.
+//  Copyright © 2016年 wangquanwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyDiary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyDiary
+//
+//  Created by 王权伟 on 2016/12/7.
+//  Copyright © 2016年 wangquanwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
